b8608e7a250019ac038850cb4f47a47b7103f715
Juan Eugenio Abadie <juaneabadie@gmail.com>
2019-10-22 12:05:23 -0300

    Add snippet to print graph (adjacency list)

---
+++ c++/graph.hpp
@@ -0,0 +1,17 @@
+#ifndef _SNIPPETS_GRAPH_HPP
+#define _SNIPPETS_GRAPH_HPP
+
+#include <iostream>
+
+template <typename AdjacencyList>
+void printGraph(const AdjacencyList& graph) {
+  for (const auto& pair: graph) {
+    std::cout << pair.first;
+    for (const auto& vertex : pair.second) {
+       std::cout << "-> " << vertex;
+    }
+    std::cout << std::endl;
+  }
+}
+
+#endif
