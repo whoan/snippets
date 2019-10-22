0390679fb8402c6544b217d5bde69fe5e996a0ac
Juan Eugenio Abadie <juaneabadie@gmail.com>
2019-10-22 11:38:31 -0300

    Add snippet to use BFS

---
+++ c++/bfs.hpp
@@ -0,0 +1,42 @@
+#ifndef _SNIPPETS_BFS_HPP
+#define _SNIPPETS_BFS_HPP
+
+template <typename GraphAdjacencyList, typename VertexType, typename Tree = std::unordered_map<VertexType, VertexType>>
+class BreadthFirstTree {
+  VertexType source;
+  Tree tree;
+
+public:
+  BreadthFirstTree(GraphAdjacencyList graph, VertexType source) : source(std::move(source)) {
+    // use BFS to create a Breadth First Tree
+    std::queue<VertexType> queue;
+    queue.push(this->source);
+
+    while (queue.size()) {
+      auto vertex = queue.front();
+      queue.pop();
+      for (const auto& adjacentVertex : graph.at(vertex)) {
+        if (!tree.count(adjacentVertex) && adjacentVertex != this->source) {
+          queue.push(adjacentVertex);
+          tree[adjacentVertex] = vertex;
+        }
+      }
+    }
+  }
+
+  int getDistance(const VertexType& target) {
+    int count = 0;
+    for (auto current = target; current != source; current = tree.at(current)) {
+      ++count;
+    }
+    return count;
+  }
+};
+
+// helper for compilers not supporting C++17 to avoid providing template parameters (C++14 is needed)
+template <typename GraphAdjacencyList, typename VertexType>
+auto makeBreadthFirstTree(GraphAdjacencyList graph, VertexType source) {
+  return BreadthFirstTree<GraphAdjacencyList, VertexType>(std::move(graph), std::move(source));
+}
+
+#endif
