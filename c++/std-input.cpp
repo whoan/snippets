7ed30eef5bf14f8596f85b0d1347d2c2c3bf9d05
Juan Eugenio Abadie <juaneabadie@gmail.com>
2019-11-04 17:35:01 -0300

    Add snippet to create collection from std input

---
+++ c++/std-input.cpp
@@ -0,0 +1,18 @@
+#ifndef _SNIPPETS_CPP_STD_INPUT_HPP
+#define _SNIPPETS_CPP_STD_INPUT_HPP
+
+#include <iostream>
+#include <vector>
+
+template <typename T>
+std::vector<T> createCollectionFromStdInput(std::size_t size) {
+  std::vector<T> collection;
+  T fromInput;
+  for (std::size_t i=0; i < size; ++i) {
+    std::cin >> fromInput;
+    collection.push_back(fromInput);
+  }
+  return collection;
+}
+
+#endif
