78321abfb937e72637e6b000aeeea01534a2c829
Juan Eugenio Abadie <juaneabadie@gmail.com>
2019-11-10 00:24:12 -0300

    Improve code

--- cpp/binary-search-tree.hpp
+++ cpp/binary-search-tree.hpp
@@ -5,20 +5,32 @@
 
 namespace snip {
 
-class Node {
+class TreeNode {
 public:
     int data;
-    Node *left;
-    Node *right;
-    Node(int d) {
-        data = d;
-        left = NULL;
-        right = NULL;
-    }
+    TreeNode* left = nullptr;
+    TreeNode* right = nullptr;
+    TreeNode(int data) : data(data) {}
 };
 
 class BFS {
-    Node* root = nullptr;
+    TreeNode* root = nullptr;
+
+    TreeNode* insert(TreeNode* rootSubTree, int data) {
+        if (rootSubTree == nullptr) {
+            return new TreeNode(data);
+        }
+
+        TreeNode* cur;
+        if (data <= rootSubTree->data) {
+            cur = insert(rootSubTree->left, data);
+            rootSubTree->left = cur;
+        } else {
+            cur = insert(rootSubTree->right, data);
+            rootSubTree->right = cur;
+        }
+        return rootSubTree;
+    }
 
 public:
     BFS(const std::vector<int>& input) {
@@ -27,24 +39,7 @@ public:
         }
     }
 
-    Node* insert(Node* rootSubTree, int data) {
-        if(rootSubTree == nullptr) {
-            return new Node(data);
-        } else {
-            Node* cur;
-            if(data <= rootSubTree->data) {
-                cur = insert(rootSubTree->left, data);
-                rootSubTree->left = cur;
-            } else {
-                cur = insert(rootSubTree->right, data);
-                rootSubTree->right = cur;
-           }
-
-           return rootSubTree;
-       }
-    }
-
-    auto getRoot() {
+    TreeNode* getRoot() {
         return root;
     }
 };
