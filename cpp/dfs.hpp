19ec1efac33cb677f608a8de12ea725552e69902
Juan Eugenio Abadie <juaneabadie@gmail.com>
2019-11-10 01:25:32 -0300

    Add snippet for DFS

---
+++ cpp/dfs.hpp
@@ -0,0 +1,44 @@
+#ifndef _SNIPPETS_DFS_HPP
+#define _SNIPPETS_DFS_HPP
+
+#include <iostream>
+
+// reference: https://www.programiz.com/dsa/tree-traversal
+
+using snip::TreeNode;
+
+namespace snip {
+
+class DFS {
+public:
+  static void preOrder(TreeNode* node) {
+    if (!node) {
+      return;
+    }
+    std::cerr << node->data << " ";
+    preOrder(node->left);
+    preOrder(node->right);
+  }
+
+  static void inOrder(TreeNode* node) {
+    if (!node) {
+      return;
+    }
+    inOrder(node->left);
+    std::cerr << node->data << " ";
+    inOrder(node->right);
+  }
+
+  static void postOrder(TreeNode* node) {
+    if (!node) {
+      return;
+    }
+    postOrder(node->left);
+    postOrder(node->right);
+    std::cerr << node->data << " ";
+  }
+};
+
+} // snip namespace
+
+#endif
