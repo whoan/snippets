099516547947e1833681c609b5d852c6bbf32d30
Juan Eugenio Abadie <juaneabadie@gmail.com>
2019-11-10 15:27:39 -0300

    Improve DFS (template) class

--- cpp/dfs.hpp
+++ cpp/dfs.hpp
@@ -5,46 +5,72 @@
 
 // reference: https://www.programiz.com/dsa/tree-traversal
 
-using snip::TreeNode;
-
 namespace snip {
 
 class DFS {
+  enum Method {
+    PreOrder, InOrder, PostOrder
+  } method;
+
+  enum Direction {
+    LeftToRight, RightToLeft
+  } direction;
+
+  template <typename TreeNode>
+  DFS(TreeNode root, Method method, Direction direction=LeftToRight)
+  : method(method), direction(direction)
+  {
+    traverse(root);
+  }
+
+  template <typename TreeNode>
+  void traverse(TreeNode node) {
+    if (!node) {
+      return;
+    }
+
+    if (method == Method::PreOrder) {
+      std::cerr << node->data << " ";
+    }
+    traverse(direction == LeftToRight ? node->left : node->right);
+    if (method == Method::InOrder) {
+      std::cerr << node->data << " ";
+    }
+    traverse(direction == LeftToRight ? node->right : node->left);
+    if (method == Method::PostOrder) {
+      std::cerr << node->data << " ";
+    }
+  }
+
 public:
-  static void preOrder(TreeNode* node) {
-    if (!node) {
-      return;
-    }
-    std::cerr << node->data << " ";
-    preOrder(node->left);
-    preOrder(node->right);
+  template <typename TreeNode>
+  static void preOrder(TreeNode root) {
+    DFS dfs(root, DFS::PreOrder);
   }
 
-  static void inOrder(TreeNode* node) {
-    if (!node) {
-      return;
-    }
-    inOrder(node->left);
-    std::cerr << node->data << " ";
-    inOrder(node->right);
+  template <typename TreeNode>
+  static void inOrder(TreeNode root) {
+    DFS dfs(root, DFS::InOrder);
   }
 
-  static void postOrder(TreeNode* node) {
-    if (!node) {
-      return;
-    }
-    postOrder(node->left);
-    postOrder(node->right);
-    std::cerr << node->data << " ";
+  template <typename TreeNode>
+  static void postOrder(TreeNode root) {
+    DFS dfs(root, DFS::PostOrder);
   }
 
-  static void reverseInOrder(TreeNode* node) {
-    if (!node) {
-      return;
-    }
-    reverseInOrder(node->right);
-    std::cerr << node->data << " ";
-    reverseInOrder(node->left);
+  template <typename TreeNode>
+  static void preOrderReverse(TreeNode root) {
+    DFS dfs(root, DFS::PreOrder, DFS::RightToLeft);
+  }
+
+  template <typename TreeNode>
+  static void inOrderReverse(TreeNode root) {
+    DFS dfs(root, DFS::InOrder, DFS::RightToLeft);
+  }
+
+  template <typename TreeNode>
+  static void postOrderReverse(TreeNode root) {
+    DFS dfs(root, DFS::PostOrder, DFS::RightToLeft);
   }
 };
 
