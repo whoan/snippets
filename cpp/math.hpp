56d7d4ac5d01944209e4e0fdf5cd7a19263fde24
Juan Eugenio Abadie <juaneabadie@gmail.com>
2019-12-30 20:58:15 -0300

    Rename file and fix indentation

--- cpp/math.hpp
+++ cpp/math.hpp
@@ -4,7 +4,7 @@ namespace snip {
 
 // double is necessary to not lose precision
 double log10(long input) {
-    return std::log(input) / std::log(10);
+  return std::log(input) / std::log(10);
 }
 
 } // snip namespace
