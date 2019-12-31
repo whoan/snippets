c9596cbb5cbda274c45862e7c46bdabcc28f09b5
Juan Eugenio Abadie <juaneabadie@gmail.com>
2019-12-30 21:18:22 -0300

    Add math snippets

--- cpp/math.hpp
+++ cpp/math.hpp
@@ -7,4 +7,12 @@ double log10(long input) {
   return std::log(input) / std::log(10);
 }
 
+int getMostSignificantDigit(int x) {
+  return x / std::pow(10, int(snip::log10(x)));
+}
+
+int getLeastSignificantDigit(int x) {
+  return x % 10;
+}
+
 } // snip namespace
