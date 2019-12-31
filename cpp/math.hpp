b37222cbed73668f2e32410fd30140985db44ff8
Juan Eugenio Abadie <juaneabadie@gmail.com>
2019-12-30 22:48:50 -0300

    Don't reinvent the wheel

--- cpp/math.hpp
+++ cpp/math.hpp
@@ -2,11 +2,6 @@
 
 namespace snip {
 
-// double is necessary to not lose precision
-double log10(long input) {
-  return std::log(input) / std::log(10);
-}
-
 int getMostSignificantDigit(int x) {
   return x / std::pow(10, int(snip::log10(x)));
 }
