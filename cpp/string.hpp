b9201fe864664746d5a4dfb848e0177e552d11a3
Juan Eugenio Abadie <juaneabadie@gmail.com>
2020-01-01 23:36:32 -0300

    Add trim snippet

--- cpp/string.hpp
+++ cpp/string.hpp
@@ -3,6 +3,22 @@
 
 namespace snip {
 
+// unsing a forwarding reference makes it possibe to pass by value or reference (rvalue)
+template<typename String>
+String trim(String&& input, char toTrim=' ') {
+    // remvove trailing characters
+    while (input.size() && input.back() == toTrim) {
+        input.resize(input.size()-1);
+    }
+
+    // remove leading characters
+    auto pos = input.find_first_not_of(toTrim);
+    if (pos != 0) {
+        input = input.substr(pos);
+    }
+    return input;
+}
+
 std::vector<std::string_view> split(std::string_view review, char separator=' ') {
     std::vector<std::string_view> words;
     std::size_t previousPos = review.find_first_not_of(separator);
